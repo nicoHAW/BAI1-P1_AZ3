f2f99a54c2dfb76deddfbe7750355e94b3a8c3d5
nicoHAW <nico.paetzel@haw-hamburg.de>
2024-07-12 13:15:18 +0200

    started

---
+++ a3x3/src/nicoTestFrame/myTestFrame.java
@@ -0,0 +1,22 @@
+package nicoTestFrame;
+
+import java.util.Arrays;
+
+public class myTestFrame {
+
+    public static void main(String[] args) {
+
+        boolean[][] myArray = {
+                {false, true, true},
+                {true, false, false, true},
+                {true, false, false, true},
+                {false, true, true},
+                };
+        
+        
+        System.out.println(Arrays.deepToString(myArray));
+        
+        
+    }
+
+}
--- a3x3/src/patternBasedArrayProcessor/ArrayProcessor.java
+++ a3x3/src/patternBasedArrayProcessor/ArrayProcessor.java
@@ -1,35 +1,46 @@
 // This source code is UTF-8 coded - see https://stackoverflow.com/questions/9180981/how-to-support-utf-8-encoding-in-eclipse
 package patternBasedArrayProcessor;
 
+import java.util.Arrays;
 
 /**
  * ArrayProcessor - see task
  * 
- * @author   (your name(s)) 
+ * @author   (Nico ) 
  * @version  (a version number or a date)
  *
  */
 public class ArrayProcessor {
     
-    //###
-    //###
-    //###
-    //###         HIER kommt Ihr Code hin ;-)
-    //###
-    //###                    VVVV
-    //###                    VVVV
-    //###                    VVVV
-    //###   VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
-    //###      VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
-    //###         VVVVVVVVVVVVVVVVVVVVVVVVVV
-    //###            VVVVVVVVVVVVVVVVVVVV
-    //###               VVVVVVVVVVVVVV
-    //###                  VVVVVVVV
-    //###                     VV
+
     public long process( long[][] theArray ){
+        
+        
+        
+        
+        boolean[][] myArray = {
+                {false, true, true},
+                {true, false, false, true},
+                {true, false, false, true},
+                {false, true, true},
+                };
+        
+        
+        System.out.println(Arrays.deepToString(myArray));
+        
+        
         
         return 0;   // <<<=== Diese Zeile ist falsch!   bzw. nur ein Platzhalter um den Compiler gluecklich zu machen. Die Zeile muss sinnvoll ersetzt werden.
     }//method()
+    
+    
+    
+    
+    
+    
+    
+    
+    
     //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
     // Führen Sie Änderungen oberhalb dieser Zeilen durch.
     //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
